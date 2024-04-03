864cdc9e55212ac6f97420644b2e18071df475ce
Augusto Chaves Fiore <augustochavesfiore@gmail.com>
2024-04-02 22:38:08 -0300

    Añadir archivos y configuraciones para el cliente y el servidor

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import { FormMatriz } from "./components/FormMatriz";
 import { useForm } from "./hooks/UseForm";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 function App() {
   const [data, setdata] = useState({
     filas: 0,
     columnas: 0,
+    filas2: 0,
+    columnas2: 0,
   });
   const { values, handleInputChange, reset } = useForm({
     filas: '',
@@ -12,18 +14,26 @@ function App() {
     filas2: '',
     columnas2: '',
   });
+  useEffect(() => {
+    reset();
+  }, [data]);
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    if(values.filas === '' || values.columnas === ''){
+    if(values.filas === '' || values.columnas === '' || values.filas2 === '' || values.columnas2 === ''){
       return alert('Ingrese un valor valido');
     };
+    if (values.filas < 0 || values.columnas < 0 || values.filas2 < 0 || values.columnas2 < 0) {
+      return alert('Ingrese un valor valido');
+    }
+
     setdata(values);
  
   };
   return (
 
       <main className="container d-flex justify-content-center align-content-center flex-column">
-        <h1 className="text-center">Suma de Matrices</h1>
+        <h1 className="text-center">Operaciones  de Matrices</h1>
         <form onSubmit={handleSubmit} className="row  justify-content-center  align-content-center">
           <div className="col-6 mb-3">
             <h3>Matriz A</h3>
@@ -48,6 +58,33 @@ function App() {
               placeholder="columnas"
               className="form-control"
               value={values.columnas}
+              {...values}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className="col-6 mb-3">
+            <h3>Matriz B</h3>
+            <label htmlFor="fila" className="form-label">
+              Filas de la Matriz
+            </label>
+            <input
+              type="number"
+              name="filas2"
+              placeholder="filas"
+              className="form-control"
+              value={values.filas2}
+              {...values}
+              onChange={handleInputChange}
+            />
+            <label htmlFor="columna" className="form-label">
+              Columnas de la matriz
+            </label>
+            <input
+              type="number"
+              name="columnas2"
+              placeholder="columnas"
+              className="form-control"
+              value={values.columnas2}
               {...values}
               onChange={handleInputChange}
             />
